08704dc8b8a712a5ede0866ad2367f7473a6ec0f
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-07-25 17:42:45 -0400

    add index for views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { PublicRoute, PrivateRoute } from './routes'
 import Navigation from './components/Navigation.jsx'
 import Footer from './components/Footer.jsx'
-import Home from './views/Home.jsx'
-import Carrito from './views/Carrito.jsx'
-import Login from './views/Login'
-import Profile from './views/Profile.jsx'
-import Register from './views/Register'
-import Product from './views/Product.jsx'
-import NewProduct from './views/NewProduct'
-import NotFound from './views/NotFound'
-import Thanks from './views/Thanks.jsx'
+import * as viewsStore from './views/index.js'
 import MichiContext from './context/MichiContex.jsx'
 import { useCart } from './hooks/useCart.jsx'
 
@@ -24,34 +16,34 @@ function App () {
         <Routes>
           <Route
             path='/'
-            element={<PublicRoute> <Home /></PublicRoute>}
+            element={<PublicRoute> <viewsStore.Home /></PublicRoute>}
           />
           <Route
             path='/carrito'
-            element={<PrivateRoute><Carrito /></PrivateRoute>}
+            element={<PrivateRoute><viewsStore.Carrito /></PrivateRoute>}
           />
           <Route
             path='/login'
-            element={<PublicRoute><Login /></PublicRoute>}
+            element={<PublicRoute><viewsStore.Login /></PublicRoute>}
           />
           <Route
             path='/register'
-            element={<PublicRoute><Register /></PublicRoute>}
+            element={<PublicRoute><viewsStore.Register /></PublicRoute>}
           />
           <Route
             path='/products/:id'
-            element={<PublicRoute><Product /></PublicRoute>}
+            element={<PublicRoute><viewsStore.Product /></PublicRoute>}
           />
           <Route
             path='/newproduct'
-            element={<PrivateRoute><NewProduct /></PrivateRoute>}
+            element={<PrivateRoute><viewsStore.NewProduct /></PrivateRoute>}
           />
           <Route
             path='/profile'
-            element={<PrivateRoute><Profile /></PrivateRoute>}
+            element={<PrivateRoute><viewsStore.Profile /></PrivateRoute>}
           />
-          <Route path='/thanks' element={<Thanks />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path='/thanks' element={<viewsStore.Thanks />} />
+          <Route path='*' element={<viewsStore.NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
