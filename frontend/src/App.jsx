7537a6ba02e41e38d678b58623ca5d3dbb552553
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-08-01 15:53:47 -0400

    add file routes.jsx for dinamic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { PublicRoute, PrivateRoute } from './routes'
 import * as components from './components'
-import * as viewsStore from './views/index'
 import MichiContext from './context/MichiContex'
 import useCart from './hooks/useCart'
+import { publicRoutes, privateRoutes } from './routes/routes'
+import PublicRoute from './routes/PublicRoute'
 
 function App () {
   const allState = useCart()
@@ -13,36 +13,22 @@ function App () {
       <BrowserRouter>
         <components.Navigation />
         <Routes>
-          <Route
-            path='/'
-            element={<PublicRoute> <viewsStore.Home /></PublicRoute>}
-          />
-          <Route
-            path='/carrito'
-            element={<PrivateRoute><viewsStore.Carrito /></PrivateRoute>}
-          />
-          <Route
-            path='/login'
-            element={<PublicRoute><viewsStore.Login /></PublicRoute>}
-          />
-          <Route
-            path='/register'
-            element={<PublicRoute><viewsStore.Register /></PublicRoute>}
-          />
-          <Route
-            path='/products/:id'
-            element={<PublicRoute><viewsStore.Product /></PublicRoute>}
-          />
-          <Route
-            path='/newproduct'
-            element={<PrivateRoute><viewsStore.NewProduct /></PrivateRoute>}
-          />
-          <Route
-            path='/profile'
-            element={<PrivateRoute><viewsStore.Profile /></PrivateRoute>}
-          />
-          <Route path='/thanks' element={<viewsStore.Thanks />} />
-          <Route path='*' element={<viewsStore.NotFound />} />
+          {publicRoutes.map(({ path, Component }) => (
+            <Route
+              key={path}
+              path={path}
+              element={<PublicRoute> <Component /> </PublicRoute>}
+            />
+          ))}
+
+          {privateRoutes.map(({ path, Component }) => (
+            <Route
+              key={path}
+              path={path}
+              element={<privateRoutes> <Component /> </privateRoutes>}
+            />
+          ))}
+
         </Routes>
         <components.Footer />
       </BrowserRouter>
---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,55 @@
+import * as views from '../views'
+
+export const publicRoutes = [
+  {
+    path: '/',
+    Component: views.Home
+
+  },
+  {
+    path: '/login',
+    Component: views.Login
+
+  },
+  {
+    path: '/register',
+    Component: views.Register
+
+  },
+  {
+    path: '/products/:id',
+    Component: views.Product
+
+  },
+
+  {
+    path: '/thanks',
+    Component: views.Thanks
+
+  },
+  {
+    path: '*',
+    Component: views.NotFound
+
+  }
+
+]
+
+export const privateRoutes = [
+  {
+    path: '/carrito',
+    Component: views.Carrito
+
+  },
+  {
+    path: '/newproduct',
+    Component: views.NewProduct
+
+  },
+  {
+    path: '/profile',
+    Component: views.Profile
+
+  }
+
+]
